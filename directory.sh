c06c450813a1d902ac20b564377f5eb5d772afb1
Bradley Stratton <108354503+SlideShowGames@users.noreply.github.com>
2024-01-24 16:40:49 -0500

    Add files via upload

---
+++ directory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd Documents/
+mkdir ChromeOS-Recovery-Images/
+cd ChromeOS-Recovery-Images/
+mkdir v111-v120-LTS-Images/
+cd ..
+cd ..
+cd ChromeOS-Recovery-Image-Archiver/
+cd Python-Scripts/
+python downloader.py
